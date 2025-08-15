2b2b320aca8a522e6c1d634cefaf65b7248a262e
arkadashiim <arkadashiim@mail.ru>
2025-08-15 18:22:46 +0300

    feat: import typeorm module for feature

--- src/articles/articles.module.ts
+++ src/articles/articles.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ArticlesService } from './articles.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Article } from './article.entity';
 import { ArticlesController } from './articles.controller';
+import { ArticlesService } from './articles.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Article])],
   providers: [ArticlesService],
-  controllers: [ArticlesController]
+  controllers: [ArticlesController],
 })
 export class ArticlesModule {}
