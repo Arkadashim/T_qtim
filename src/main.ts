013f8f8d8e31b5f09bed8b96a13a5810206cdb2e
arkadashiim <arkadashiim@mail.ru>
2025-08-15 20:32:15 +0300

    chore: swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  // Для непроодвских окружений подключаем Swagger
+  if (configService.get('NODE_ENV') !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('Тестовое задание QTIM')
+      .setDescription('API для управления пользователями и статьями')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/', app, document);
+  }
 
   // Глобальные пайпы
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
