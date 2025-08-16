7b4c965a2bdcc35ff678105290a07c3e92486372
arkadashiim <arkadashiim@mail.ru>
2025-08-16 21:39:44 +0300

    feat: transform on response

--- src/articles/dto/article-response.dto.ts
+++ src/articles/dto/article-response.dto.ts
@@ -1,4 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 
 export class ArticleResponseDto {
   @ApiProperty({ example: 1, description: 'Уникальный идентификатор статьи' })
@@ -14,6 +15,12 @@ export class ArticleResponseDto {
   description: string;
 
   @ApiProperty({ example: '2025-08-15', description: 'Дата публикации статьи' })
+  // Трансформация даты в строку ISO при передаче ответа
+  @Transform(({ value }) =>
+    value instanceof Date
+      ? value.toISOString().split('T')[0]
+      : (value as string),
+  )
   publicationDate: string;
 
   @ApiProperty({ example: 1, description: 'ID автора статьи' })
