a584b3ea5c9a71b787c5cad0f55dd7bda76ec7d9
arkadashiim <arkadashiim@mail.ru>
2025-08-17 00:46:16 +0300

    refactor: rename var

--- src/constants/cache.ts
+++ src/constants/cache.ts
@@ -1,5 +1,5 @@
 export const ARTICLE_CACHE_KEYS = {
   BY_ID: (id: number) => `article_${id}`,
-  ALL_PAGINATED: (page: number, limit: number) =>
+  PAGINATED: (page: number, limit: number) =>
     `articles_page_${page}_limit_${limit}`,
 };
